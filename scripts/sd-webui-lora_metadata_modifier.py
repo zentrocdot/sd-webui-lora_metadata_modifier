a55ba68c2bc9e9301b8775b78bde1872a6da76b3
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-28 10:48:30 +0100

    Update sd-webui-lora_metadata_modifier.py

--- scripts/sd-webui-lora_metadata_modifier.py
+++ scripts/sd-webui-lora_metadata_modifier.py
@@ -234,9 +234,15 @@ def on_ui_tabs():
                 src_path = _lora_dict.get(src)
                 dst_path = ''.join([src_path, ".bak"])
                 shutil.copyfile(src_path, dst_path)
+                
+                key1 = "ss_tag_frequency"
+                temp_value = metadata.get(key1)
+                temp_value = json.dumps(temp_value)
+                metadata.update({key1:temp_value})
+
                 json_object = json.loads(metadata)
-                temp_value = json.dumps(json_object)
-                json_object = json.loads( temp_value)
+                #temp_value = json.dumps(json_object)
+                #json_object = json.loads( temp_value)
                 retcode = write_metadata(dst_path, src_path, json_object)
                 #retcode = write_metadata(dst_path, src_path, temp_value)
                 if retcode == 0:
